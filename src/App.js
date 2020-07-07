3842e8ff75af40ab1782c3595786e8b510cc4d12
Carmen Diaz <cdechauri@gmail.com>
2020-07-06 20:52:28 -0700

    Homepage in broken components
    - Also finished styling menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>E-stuffing</h1>
+        <h1>E-Stuffing</h1>
+        <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.scss';
+import Category from "../components/Category/Category";
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Category />
+    </div>
+);
+
+export default HomePage;
